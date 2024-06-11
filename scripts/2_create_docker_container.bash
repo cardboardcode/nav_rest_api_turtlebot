01e015c28e126cd1287a929a250d82374616ae43
Bey Hao Yun <beyhy94@gmail.com>
2024-06-11 21:57:09 +0800

    :heavy_plus_sign: Added easeofuse scripts. Temporary setup for now. Will modify to suit immediate running of packages from docker rather than current manual running.
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

---
+++ scripts/2_create_docker_container.bash
@@ -0,0 +1,7 @@
+docker kill ros1_turtlebot3_rest_api_c
+docker rm ros1_turtlebot3_rest_api_c
+
+docker run -it \
+ --name ros1_turtlebot3_rest_api_c \
+ --net=host \
+ ros1_turtlebot3_rest_api:latest /bin/bash
---
+++ scripts/3_access_docker_container.bash
@@ -0,0 +1,3 @@
+docker start ros1_turtlebot3_rest_api_c
+
+docker exec -it ros1_turtlebot3_rest_api_c bash
